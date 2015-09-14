a692d597386b8df77570fa0ce7c7944ad6f01aa5
Aleksander Korolev <a_korolev@wargaming.net>
2015-09-14 15:33:18 +0300

    Add compile exec in template

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var loaderUtils = require('loader-utils');
 var Hogan = require('hogan.js');
 var minifier = require('html-minifier');
@@ -16,6 +14,7 @@ var minifierDefaults = {
 
 // :)
 var extend = function(target, source) {
+    'use strict';
     target = JSON.parse(JSON.stringify(target));
 
     Object.keys(source).forEach(function(key) {
@@ -26,6 +25,7 @@ var extend = function(target, source) {
 };
 
 module.exports = function(source) {
+    'use strict';
     var query = loaderUtils.parseQuery(this.query);
 
     if (this.cacheable) {
@@ -46,18 +46,43 @@ module.exports = function(source) {
     }
 
     var suffix;
+
     if (query.noShortcut) {
         suffix = 'return T; }();';
     } else {
         suffix = 'return T.render.apply(T, arguments); };';
     }
-
+    var replaced = {};
+    var vdata = source.replace(/\{\{(.+?)\}\}/gi, function(full, matched, pos, string) {
+        var repl;
+        repl = (matched.indexOf('_(')<0)?false:"{{ $$$"+pos+"$$$ }}";
+        if (repl) {
+            //console.log(repl);
+            replaced[pos] = matched;
+        }
+        return repl || full;
+    });
     var compile = 'var H = require("hogan.js");\n' +
            'module.exports = function() { ' +
            'var T = new H.Template(' +
-           Hogan.compile(source, { asString: true }) +
+           Hogan.compile(vdata, { asString: true }) +
            ', ' +
-           JSON.stringify(source) +
+           JSON.stringify(vdata) +
            ', H);' + suffix;
-    return compile.replace(/t\.[fd]\(\"_\(([^\"]+)\)\"[^\)]+\)/gi, 'gettext($1)');
+    var compile = compile.replace(/t\.f\(\"\$\$\$(\d+)\$\$\$\"\,c\,p\,0\)/gi, function(full, matched, pos, string) {
+        'use strict';
+        var source = replaced[matched];
+        var percentPos = source.indexOf(' % ');
+        var str = percentPos<0?source:source.substring(0,percentPos);
+        str = str.replace(/\|\w+$/gi, '');
+        var data = percentPos<0?false:source.substring(percentPos+3, source.length);
+        str = str.replace(/\_\(\'(.+)\'\)/gi, 'gettext("$1")');
+        if (data) {
+            data = data.replace(/\|\w+([\,\}])/gi, '$1');
+            data = data.replace(/\: ([^\']+)([\,\}]) /gi, ': p.$1$2');
+            str = 'compile('+str+', '+data+')';
+        }
+        return str;
+    })
+    return compile;
 };
