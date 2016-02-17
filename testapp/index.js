33c73833f7f61ac0362b0d0e909ce2b6c4ad7a77
Aleksandr Korolev <aleksandr@korolev.email>
2016-02-17 19:52:14 +0300

    fix repository urls

--- testapp/index.js
+++ testapp/index.js
@@ -1,10 +1,10 @@
-import $ from 'jquery';
 import testTemplate from './template.html';
 import testTemplate1 from './template1.html';
 
 var render = function(context = {}) {
-    $('body').append(testTemplate(context));
-    $('body').append(testTemplate1(context));
+    document.querySelector('body').append(testTemplate(context))
+    //$('body').append(testTemplate(context));
+    //$('body').append(testTemplate1(context));
 }
 
 export default render;
